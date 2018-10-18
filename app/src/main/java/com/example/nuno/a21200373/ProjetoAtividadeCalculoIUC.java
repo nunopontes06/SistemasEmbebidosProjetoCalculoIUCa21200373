34a405aecffb77dda1aca6b43b6da20f47f1d7ef
nunopontes06 <nunonogueira06@gmail.com>
2018-10-18 22:05:06 +0100

    Initial commit

---
+++ app/src/main/java/com/example/nuno/a21200373/ProjetoAtividadeCalculoIUC.java
@@ -0,0 +1,13 @@
+package com.example.nuno.a21200373;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProjetoAtividadeCalculoIUC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_projeto_atividade_calculo_iuc);
+    }
+}
